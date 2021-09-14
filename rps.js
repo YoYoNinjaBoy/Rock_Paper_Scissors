e079da0631938453416b9a1fade10ab17f9a394e
Cole Mickelson <cmickelson94@gmail.com>
2021-09-14 17:45:02 -0500

    single round rps working

--- rps.js
+++ rps.js
@@ -5,11 +5,23 @@ if (rng===0) {computerChoice='rock'}
 else if (rng===1) {computerChoice='paper'}
 else if (rng===2) {computerChoice='scissors'}
 return computerChoice
-}//plays rps in the console
+}//plays rps in the console type round('rock') to throw rock round('paper') for paper etc.
 function round(playerSelection, computerSelection=computerPlay()){
-if (playerSelection==='rock' && computerSelection===rock ||
-    playerSelection==='paper' && computerSelection==='paper' ||
-    playerSelection==='scissors' && computerSelection==='scissors')
+if (playerSelection.toLowerCase()==='rock' && computerSelection==='rock' ||
+    playerSelection.toLowerCase()==='paper' && computerSelection==='paper' ||
+    playerSelection.toLowerCase()==='scissors' && computerSelection==='scissors')
 {return 'tie game!'}
-else {return 'go code more'}
+else if ( 
+    playerSelection.toLowerCase()==='rock' && computerSelection==='paper' ||
+    playerSelection.toLowerCase()==='paper' && computerSelection==='scissors' ||
+    playerSelection.toLowerCase()==='scissors' && computerSelection==='rock'
+    )
+    {return 'you lose!'}
+else if (
+    playerSelection.toLowerCase()==='rock' && computerSelection==='scissors' ||
+    playerSelection.toLowerCase()==='paper' && computerSelection==='rock' ||
+    playerSelection.toLowerCase()==='scissors' && computerSelection==='paper'
+)
+{return 'you win!'}
+else {return 'uh oh'}
 }
