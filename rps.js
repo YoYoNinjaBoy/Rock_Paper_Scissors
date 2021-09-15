9a9163a8960e27172a27693afb4edfe4202a9b70
Cole Mickelson <cmickelson94@gmail.com>
2021-09-14 23:08:27 -0500

    game function works properly for single round

--- rps.js
+++ rps.js
@@ -7,6 +7,7 @@ else if (rng===2) {computerChoice='scissors'}
 return computerChoice
 }//plays rps in the console type round('rock') to throw rock round('paper') for paper etc.
 function round(playerSelection, computerSelection=computerPlay()){
+    playerSelection=window.prompt('type rock, paper, or scissors');
 if (playerSelection.toLowerCase()==='rock' && computerSelection==='rock' ||
     playerSelection.toLowerCase()==='paper' && computerSelection==='paper' ||
     playerSelection.toLowerCase()==='scissors' && computerSelection==='scissors')
@@ -24,4 +25,15 @@ else if (
 )
 {return 'you win!'}
 else {return 'uh oh'}
+}//play 5 rounds and display results after each round no workie i bad
+function game(){
+    let wins=0;
+    let losses=0;
+    let ties=0;
+    let results=round(5);
+    if (results==='you win!') {wins++}
+    else if (results==='you lose!') {losses++}
+    else if (results==='tie game!') {ties++}
+    let score='wins:'+wins+' losses:'+losses+' ties:'+ties;
+    return score
 }
