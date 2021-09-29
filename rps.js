023214c57e8e10fb16bfcf7136ad93e74c52c3df
Cole Mickelson <cmickelson94@gmail.com>
2021-09-28 19:59:58 -0500

    full of spaghetti but progress made?

--- rps.js
+++ rps.js
@@ -1,3 +1,49 @@
+const p3=document.querySelector('#p3');
+
+function gimmeButtons(){
+    startGameBtn.remove()
+
+
+    const gbContainer=document.querySelector('#gameButtons');
+   
+    const rockBtn=document.createElement('button');
+        rockBtn.innerHTML='rock';
+        gbContainer.appendChild(rockBtn);
+        rockBtn.onclick=() =>
+        p3.textContent=p3.textContent.replace(p3.textContent,
+            round('rock'))
+            ;
+
+    const paperBtn=document.createElement('button');
+        paperBtn.innerHTML='paper';
+        gbContainer.appendChild(paperBtn);
+        paperBtn.onclick=() =>
+            p3.textContent=p3.textContent.replace(p3.textContent,
+            round('paper'))
+            ;
+
+    const scissorsBtn=document.createElement('button');
+        scissorsBtn.innerHTML='scissors';
+        gbContainer.appendChild(scissorsBtn);
+        const p3=document.querySelector('#p3');
+        scissorsBtn.onclick=() =>
+            p3.textContent=p3.textContent.replace(p3.textContent,
+                round('scissors'))
+                ;
+}
+
+const startGameBtn=document.querySelector('#startGame');
+    startGameBtn.onclick=()=>gimmeButtons();
+//const rockBtn=document.querySelector('#rockBtn');
+  //  rockBtn.onclick=() =>round('rock');
+        ;
+
+//const paperBtn=document.querySelector('#paperBtn');
+  //  paperBtn.onclick=() =>round('paper');
+
+//const scissorsBtn=document.querySelector('#scissorsBtn');
+  //  scissorsBtn.onclick=()=>round('scissors');
+
 //randomly generates rock, paper, or scissors
 function computerPlay(){
 let rng=Math.floor(Math.random() *3);
@@ -5,29 +51,32 @@ if (rng===0) {computerChoice='rock'}
 else if (rng===1) {computerChoice='paper'}
 else if (rng===2) {computerChoice='scissors'}
 return computerChoice
-}//plays rps in the console type round('rock') to throw rock round('paper') for paper etc.
-function round(playerSelection, computerSelection=computerPlay()){
-    playerSelection=window.prompt('type rock, paper, or scissors');
+
+}
+//plays rps in the console type round('rock') to throw rock round('paper') for paper etc.
+function round(playerSelection,computerSelection=computerPlay()){
 if (playerSelection.toLowerCase()==='rock' && computerSelection==='rock' ||
     playerSelection.toLowerCase()==='paper' && computerSelection==='paper' ||
     playerSelection.toLowerCase()==='scissors' && computerSelection==='scissors')
 {return 'tie game!'}
+
 else if ( 
     playerSelection.toLowerCase()==='rock' && computerSelection==='paper' ||
     playerSelection.toLowerCase()==='paper' && computerSelection==='scissors' ||
     playerSelection.toLowerCase()==='scissors' && computerSelection==='rock'
     )
     {return 'you lose!'}
+
 else if (
     playerSelection.toLowerCase()==='rock' && computerSelection==='scissors' ||
     playerSelection.toLowerCase()==='paper' && computerSelection==='rock' ||
     playerSelection.toLowerCase()==='scissors' && computerSelection==='paper'
 )
 {return 'you win!'}
-else {return 'typo'}
+
 }//play 5 rounds and display results after each round
-function game()
-{
+function game(){
+
     let ties=0,
         wins=0,
         losses=0;
@@ -42,5 +91,5 @@ function game()
     }
     if (wins>losses) {alert('You win!')}
     else if(wins<losses) {alert('You Lose!')}
-    else  {alert('uh oh')}
+    else  {alert('uh oh, something went wrong.')}
 }
